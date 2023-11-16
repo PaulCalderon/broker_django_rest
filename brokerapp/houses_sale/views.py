b7e53258199b7427cb17558eb320928f49964bab
PaulCalderon <117964753+PaulCalderon@users.noreply.github.com>
2023-11-17 02:17:41 +0800

    commit initial views.py

--- brokerapp/houses_sale/views.py
+++ brokerapp/houses_sale/views.py
@@ -1,6 +1,8 @@
 
 from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from .models import HouseList
 from .serializers import HouseSerializer
@@ -9,47 +11,45 @@ from .serializers import HouseSerializer
 
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def house_list(request):
     """
     List all code snippets, or create a new snippet.
     """
     if request.method == 'GET':
-        snippets = HouseList.objects.all()
-        serializer = HouseSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        houses = HouseList.objects.all()
+        serializer = HouseSerializer(houses, many=True)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = HouseSerializer(data=data)
+        serializer = HouseSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def house_detail(request, pk):
     """
     Retrieve, update or delete a code snippet.
     """
     try:
-        snippet = HouseList.objects.get(pk=pk)
+        house = HouseList.objects.get(pk=pk)
     except HouseList.DoesNotExist:
         return HttpResponse(status=404)
 
     if request.method == 'GET':
-        serializer = HouseSerializer(snippet)
-        return JsonResponse(serializer.data)
+        serializer = HouseSerializer(house)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = HouseSerializer(snippet, data=data)
+        serializer = HouseSerializer(house, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
-        snippet.delete()
-        return HttpResponse(status=204)
+        house.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
